0f43cfdbee316186e01e91788a464603cd8a9445
T3k5upp0rt <kylan.childers@gmail.com>
2019-07-18 11:40:36 -0700

    Created object variables for trivia questions, answers, and options

---
+++ assets/javascript/game.js
@@ -0,0 +1,42 @@
+var triviaQuestions = {
+    q1: "Mt Baker is located in which state?",
+    q2: "Which state is the sunshine state?",
+    q3: "Which state has the lowest population density?",
+    q4: "Which state has the smallest population?",
+    q5: "Which state gets the most rainfall?",
+    q6: "Which state has the highest population density?",
+    q7: "Which state has the largest population?",
+    q8: "Which state is the largest?",
+    q9: "Which state is the smallest?",
+    q10: "Which state gets the most snowfall"
+}
+
+var triviaOptions = {
+    q1: ["Oregon", "California", "Washington", "Hawaii"],
+    q2: ["California", "Florida", "Texas", "Arizona"],
+    q3: ["Wisconsin", "Wyoming", "Alaska", "South Dakota"],
+    q4: ["Vermont", "Wyoming", "New Hampshire", "Alaska"],
+    q5: ["Florida", "Washington", "Mississippi", "Hawaii"],
+    q6: ["New Jersey", "New York", "Rhode Island", "Texas"],
+    q7: ["Texas", "New York", "Florida", "California"],
+    q8: ["Texas", "California", "Alaska", "Montana"],
+    q9: ["Rhode Island", "Deleware", "Hawaii", "Connecticut"],
+    q10: ["Maine", "Alaska", "Colorado", "Vermont"]
+}
+
+var triviaAnswers = {
+    q1: "Washington",
+    q2: "Florida",
+    q3: "Alaska",
+    q4: "Wyoming",
+    q5: "Hawaii",
+    q6: "New Jersey",
+    q7: "California",
+    q8: "Alaska",
+    q9: "Rhode Island",
+    q10: "Vermont"
+}
+
+var questionNumber = 0;
+var numberRight = 0;
+var numberWrong = 0;
